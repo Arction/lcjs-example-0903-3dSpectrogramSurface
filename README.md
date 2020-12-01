40094119b284a932a32d3ef3a8e0f18ac7749399
snekw <dev.snekw@gmail.com>
2020-12-01 15:14:53 +0200

    Update to LCJS 2.1.0

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
+# Spectrogram Surface 3D
+
+![Spectrogram Surface 3D](3dSpectrogramSurface.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+This example shows a use case for 3D Surface Series as a 3D *spectrogram*
+
+Spectrogram is a visual representation of the spectrum of frequencies. Spectrograms can be used to visualize any wave form. Most often spectrograms are used to display audio signals.
+
+The spectrogram shows frequency on one axis (X Axis) and time on the other (Z Axis). The amplitude of the frequency is displayed both by the Y coordinate as well as the dynamically applied *color*.
+
+
+## API Links
+
+* [3D chart]
+* [3D surface grid series]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+© Arction Ltd 2009-2020. All rights reserved.
+
+
+[3D chart]: https://www.arction.com/lightningchart-js-api-documentation/v2.1.0/classes/chart3d.html
+[3D surface grid series]: https://www.arction.com/lightningchart-js-api-documentation/v2.1.0/classes/surfacegridseries3d.html
 
---
+++ src/index.js
@@ -0,0 +1,161 @@
+/*
+ * LightningChartJS example that showcases Surface Grid Series 3D to render a 3D spectrogram.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    AxisTickStrategies,
+    ColorRGBA,
+    LUT,
+    PalettedFill,
+    SurfaceSeriesTypes3D,
+    AxisScrollStrategies,
+    UIElementBuilders,
+    UIOrigins,
+    UILayoutBuilders,
+    UIBackgrounds,
+    Themes
+} = lcjs
+
+const {
+    createSpectrumDataGenerator
+} = require('@arction/xydata')
+
+
+// Length of single data sample.
+const dataSampleSize = 60
+
+// Length of data history.
+const dataHistoryLength = 50
+
+
+// Setup PalettedFill for dynamically coloring Surface by Y coordinate.
+const lut = new LUT( {
+    steps: [
+        { value: 15, color: ColorRGBA( 4, 11, 125 ) },
+        { value: 30, color: ColorRGBA( 4, 130, 5 ) },
+        { value: 60, color: ColorRGBA( 132, 15, 4 ) },
+        { value: 100, color: ColorRGBA( 255, 255, 0 ) }
+    ],
+    interpolate: true
+} )
+const paletteFill = new PalettedFill( { lut, lookUpProperty: 'y' } )
+
+
+// Create Chart3D and configure Axes.
+const chart3D = lightningChart().Chart3D({
+    // theme: Themes.dark
+})
+    .setTitle( '3D Surface Grid Spectrogram' )
+    .setBoundingBox( { x: 1, y: 1, z: 2 } )
+chart3D.getDefaultAxisY()
+    .setScrollStrategy( AxisScrollStrategies.expansion )
+    .setInterval( 0, 100 )
+    .setTitle( 'Power spectrum P(f)' )
+chart3D.getDefaultAxisX()
+    .setTitle( 'Frequency (Hz)' )
+chart3D.getDefaultAxisZ()
+    .setTitle( 'Time' )
+    .setTickStrategy( AxisTickStrategies.Empty )
+    .setInterval( 0, dataHistoryLength )
+
+
+// Create Surface Grid Series.
+// pixelate: true adds one extra col+row to Surface, offset that here.
+const rows = dataHistoryLength - 1
+const columns = dataSampleSize - 1
+
+const surfaceGridSeries = chart3D.addSurfaceSeries( {
+    type: SurfaceSeriesTypes3D.Grid,
+    rows,
+    columns,
+    start: { x: 0, z: dataHistoryLength },
+    end: { x: dataSampleSize, z: 0 },
+    pixelate: true
+} )
+    .setFillStyle( paletteFill )
+
+
+// Setup data streaming.
+let samplesAmount = 0
+createSpectrumDataGenerator()
+    .setSampleSize( dataSampleSize )
+    .setNumberOfSamples( dataHistoryLength )
+    .setVariation( 3 )
+    .generate()
+    .setStreamRepeat( true )
+    .setStreamInterval( 1000 / 60 )
+    .setStreamBatchSize( 1 )
+    .toStream()
+    // Scale Y values from [0.0, 1.0] to [0.0, 80]
+    .map( sample => sample.map( y => y * 80 ) )
+    // Push Y values to Surface Grid as Rows.
+    .forEach( sample => {
+        const infiniteStreamingDataEnabled = toggleStreamingCheckBox.getOn()
+        const addSample = infiniteStreamingDataEnabled || samplesAmount < dataHistoryLength
+        if ( addSample ) {
+            surfaceGridSeries.addRow( 1, 'y', [sample] )
+            samplesAmount ++
+        }
+    } )
+
+
+// Animate Camera movement from file.
+;(async () => {
+    const cameraAnimationData = await (
+        fetch( document.head.baseURI + 'examples/assets/lcjs_example_0903_3dSpectrogramSurface-camera.json' )
+            .then( r => r.json() )
+    )
+    if ( ! cameraAnimationData ) {
+        console.log(`No Camera animation data.`)
+        return
+    }
+    console.log(`Loaded Camera animation data.`)
+    let frame = 0
+    const nextFrame = () => {
+        if ( cameraAnimationEnabledCheckbox.getOn() ) {
+            const { cameraLocation } = cameraAnimationData.frames[Math.floor(frame) % cameraAnimationData.frames.length]
+            chart3D.setCameraLocation( cameraLocation )
+            frame += 4
+        }
+        requestAnimationFrame( nextFrame )
+    }
+    requestAnimationFrame( nextFrame )
+})()
+
+
+
+// * UI controls *
+const group = chart3D.addUIElement( UILayoutBuilders.Column
+    .setBackground( UIBackgrounds.Rectangle )
+)
+group
+    .setPosition( { x: 0, y: 100 } )
+    .setOrigin( UIOrigins.LeftTop )
+    .setMargin( 10 )
+    .setPadding( 4 )
+
+// Add UI control for toggling between infinite streaming data and static data amount.
+const handleStreamingToggled = ( state ) => {
+    toggleStreamingCheckBox.setText( state ? 'Disable infinite streaming data' : 'Enable infinite streaming data' )
+    if ( toggleStreamingCheckBox.getOn() !== state ) {
+        toggleStreamingCheckBox.setOn( state )
+    }
+}
+const toggleStreamingCheckBox = group.addElement( UIElementBuilders.CheckBox )
+toggleStreamingCheckBox.onSwitch(( _, state ) => handleStreamingToggled( state ) )
+handleStreamingToggled( true )
+
+// Add UI control for toggling camera animation.
+const handleCameraAnimationToggled = ( state ) => {
+    cameraAnimationEnabledCheckbox.setText( state ? 'Disable camera animation' : 'Enable camera animation' )
+    if ( cameraAnimationEnabledCheckbox.getOn() !== state ) {
+        cameraAnimationEnabledCheckbox.setOn( state )
+    }
+}
+const cameraAnimationEnabledCheckbox = group.addElement( UIElementBuilders.CheckBox )
+cameraAnimationEnabledCheckbox.onSwitch((_, state) => handleCameraAnimationToggled( state ))
+handleCameraAnimationToggled( true )
